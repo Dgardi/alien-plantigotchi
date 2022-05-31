6ab8a3e5b7a5c55b4056970792f0a1b3d29a2b81
Devyn Blackstone <devynblackmon@gmail.com>
2022-05-31 11:52:01 -0500

    updated started code

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const path = require('path');
-const db = require('./config/connection');
+const express = require("express");
+const path = require("path");
+const db = require("./config/connection");
 // const routes = require('./routes');
 
 const app = express();
@@ -10,12 +10,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/"));
+});
 // app.use(routes);
 
-db.once('open', () => {
+db.once("open", () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
