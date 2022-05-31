42ae4785a781958e8e408e3c23e63a4d83f6bf46
Sarah Gilbreath <thenoiseinspace@gmail.com>
2022-05-31 11:17:00 -0400

    initializing component files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+import React,{ useState } from 'react';
+// import Navbar from './components/Navbar';
+import LoginForm from './components/LoginForm'; 
+import ActionButtons from './components/ActionButtons';
+import AlienPlant from "./components/AlienPlant"
+import HumanPlant from './components/HumanPlant';
+import Greenhouse from "./components/Greenhouse"; 
+import LoginPage from "./components/LoginPage"; 
+
+// In our main App component, we are rendering only single instances of Header and Navbar and several instances of Card
 function App() {
+  const [selectedPage, setSelectedPage] = useState("LoginPage")
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header setSelectedPage={setSelectedPage} />
+      {/* <NavTabs setSelectedPage = {setSelectedPage} sarah={"test"}/> */}
+      {selectedPage === "Portfolio" && <Portfolio/>}
+      {selectedPage === "About" && <About/>}
+      {selectedPage === "Resume" && <Resume />}
+      {selectedPage === "Contact" && <Contact />}
+
+      <Footer />
     </div>
   );
 }
 
 export default App;
+
---
+++ src/components/LoginForm.js
@@ -0,0 +1,91 @@
+import React, { useState } from 'react';
+//need to create styles and link here
+import '../styles/styles.css';
+
+
+export default function Login () {
+    // Here we set two state variables for firstName and lastName using `useState`
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [emailSubmission, setEmailSubmission] = useState('');
+    const [messageSubmission, setMessageSubmission] = useState('');
+  
+    const handleInputChange = (e) => {
+      // Getting the value and name of the input which triggered the change
+      const { name, value } = e.target;
+    //   console.log(e.target)
+  
+      switch(name) {
+        case "userName":
+            userName(value)
+          break;
+        case "emailSubmission":
+            setEmailSubmission(value)
+          break;
+        case "passwordSubmission":
+            setPasswordSubmission(value)
+          break;
+        default:
+      }
+
+    return null; 
+    };
+
+  
+    const handleFormSubmit = (e) => {
+      // Preventing the default behavior of the form submit (which is to refresh the page)
+      e.preventDefault();
+
+
+        var pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
+      
+        if (!pattern.test(emailSubmission)) {
+            document.getElementById("errors").style="display: block"
+      } else {
+        document.getElementById("errors").style="display: none"
+      }
+
+    };
+  
+    return (
+      <div>
+        <p>
+          Get in touch: 
+        </p>
+        <form className="form">
+          <input
+            value={userName}
+            name="userName"
+            onChange={handleInputChange}
+            type="text"
+            placeholder="First Name"
+          />
+          <br></br>
+          <input
+            value={emailSubmission}
+            name="emailSubmission"
+            onChange={handleInputChange}
+            type="email"
+            placeholder="Email"
+          />
+          <br></br>
+          <div id="errors">
+            <p>Invalid email</p>
+          </div>
+          <input
+            value={passwordSubmission}
+            name="passwordSubmission"
+            onChange={handleInputChange}
+            //DO WE NEED TO CHANGE THE TYPE???
+            type="text"
+            placeholder="Password"
+          />
+          <br></br>
+          <button type="button" onClick={handleFormSubmit}>
+            Submit
+          </button>
+        </form>
+      </div>
+    );
+  }
+  
