b86cce24a369222e5fcb557af120a98e1ebe2236
Robert Slocumbe <rslocumbe93@gmail.com>
2022-06-02 10:47:14 -0400

    prepping for pull

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -6,7 +6,6 @@ import React, { useState } from 'react';
 export default function Login () {
     // Here we set two state variables for firstName and lastName using `useState`
     const [userName, setUserName] = useState('');
-    const [emailSubmission, setEmailSubmission] = useState('');
     const [passwordSubmission, setPasswordSubmission] = useState('');
   
     const handleInputChange = (e) => {
@@ -17,9 +16,6 @@ export default function Login () {
       switch(name) {
         case "userName":
             setUserName(value)
-          break;
-        case "emailSubmission":
-            setEmailSubmission(value)
           break;
         case "passwordSubmission":
             setPasswordSubmission(value)
@@ -35,9 +31,6 @@ export default function Login () {
       // Preventing the default behavior of the form submit (which is to refresh the page)
       e.preventDefault();
 
-
-        var pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
-      
         if (!pattern.test(emailSubmission)) {
             document.getElementById("errors").style="display: block"
       } else {
@@ -60,17 +53,6 @@ export default function Login () {
             placeholder="First Name"
           />
           <br></br>
-          <input
-            value={emailSubmission}
-            name="emailSubmission"
-            onChange={handleInputChange}
-            type="email"
-            placeholder="Email"
-          />
-          <br></br>
-          <div id="errors">
-            <p>Invalid email</p>
-          </div>
           <input
             value={passwordSubmission}
             name="passwordSubmission"
