c101a3e1b849411a583de7b9aff31801ffa7c59d
Sarah Gilbreath <thenoiseinspace@gmail.com>
2022-06-02 14:37:08 -0400

    life points are updating

--- client/src/components/AlienActionButtons.js
+++ client/src/components/AlienActionButtons.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 function AlienActionButtons(props) {
-  const {setSelectedPage} = props
-  const clickHandler = (event) => {
-    setSelectedPage(event.target.value)
-    console.log(event.target.value)
-  }
+  // const {setSelectedPage} = props
+  // const clickHandler = (event) => {
+  //   setSelectedPage(event.target.value)
+  //   console.log(event.target.value)
+  // }
   return (
     <p>This plant's name is: </p>
   );
--- client/src/components/AlienPlant.js
+++ client/src/components/AlienPlant.js
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 // import alienPlant_stage1 from "../../src/assets/alienPlant_stage1.png"
 // import alienPlant_stage2 from "../../src/assets/alienPlant_stage2.png"
 // import alienPlant_stage3 from "../../src/assets/alienPlant_stage3.png"
@@ -8,7 +9,9 @@ export default function AlienPlant ({stage}) {
 
     let photoArray = ["https://plantigotchi.s3.us-east-2.amazonaws.com/alienPlant_stage1.png", "https://plantigotchi.s3.us-east-2.amazonaws.com/alienPlant_stage2.png", "https://plantigotchi.s3.us-east-2.amazonaws.com/alienPlant_stage3.png", "https://plantigotchi.s3.us-east-2.amazonaws.com/alienPlant_stage4.png", "https://plantigotchi.s3.us-east-2.amazonaws.com/alienPlant_stage5.png"]; 
     console.log(stage)
-        
+  
+     // Declare a new state variable, which we'll call "count"
+    const [lifePoints, setLifePoints] = useState(0);
     
     const expr = () => {
                 switch ("AlienStage1") {
@@ -16,35 +19,38 @@ export default function AlienPlant ({stage}) {
                     console.log('stage 1')
                     return(
                     <div>
-                        <button type="button">Water</button>
-                        <button type="button">Sun</button>
-                        <button type="button">Fertilizer</button>
-                        <button type="button">Sing</button>
+                        <button type="button" onClick={() => setLifePoints(lifePoints + Math.floor(Math.random()*5))}>Water</button>
+                        <button type="button"onClick={() => setLifePoints(lifePoints + Math.floor(Math.random()*5))}>Sun</button>
+                        <button type="button"onClick={() => setLifePoints(lifePoints + Math.floor(Math.random()*5))}>Fertilizer</button>
+                        <button type="button"onClick={() => setLifePoints(lifePoints + Math.floor(Math.random()*5))}>Sing</button>
+                        <p>You have {lifePoints} life points</p>
                     </div>)
                 case 'AlienStage2':
                     console.log('stage 2');
                     return(
                     <div>
-                        <button type="button">Water</button>
-                        <button type="button">Sun</button>
-                        <button type="button">Fertilizer</button>
-                        <button type="button">Sing</button>
+                        <button type="button" onClick={() => setLifePoints(lifePoints + Math.floor(Math.random()*5))}>Water</button>
+                        <button type="button"onClick={() => setLifePoints(lifePoints + Math.floor(Math.random()*5))}>Sun</button>
+                        <button type="button"onClick={() => setLifePoints(lifePoints + Math.floor(Math.random()*5))}>Fertilizer</button>
+                        <button type="button"onClick={() => setLifePoints(lifePoints + Math.floor(Math.random()*5))}>Sing</button>
+                        <p>You have {lifePoints} life points</p>
                     </div>)
                 case 'AlienStage3':
                     console.log('stage 3');
                     return(
                     <div>
-                        <button type="button">Water</button>
-                        <button type="button">Sun</button>
-                        <button type="button">Fertilizer</button>
-                        <button type="button">Sing</button>
+                        <button type="button" onClick={() => setLifePoints(lifePoints + Math.floor(Math.random()*5))}>Water</button>
+                        <button type="button"onClick={() => setLifePoints(lifePoints + Math.floor(Math.random()*5))}>Sun</button>
+                        <button type="button"onClick={() => setLifePoints(lifePoints + Math.floor(Math.random()*5))}>Fertilizer</button>
+                        <button type="button"onClick={() => setLifePoints(lifePoints + Math.floor(Math.random()*5))}>Sing</button>
+                        <p>Your plant has {lifePoints} life points</p>
                     </div>)
                 default:
                     console.log(`Sorry, an error occured loading the actions.`);
                     }
                 }
     
-    
+
     return(
             <div>
               <img src={photoArray[stage]} className="alienPlantPic" alt="AlienPlant in various stages"></img>  
