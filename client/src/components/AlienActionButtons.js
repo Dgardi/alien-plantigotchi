0f838366c587674fcbcb17a737483c444aee2317
David Gardiner <dgardi@gmail.com>
2022-06-03 14:51:12 -0400

    stashing some stuff

--- client/src/components/AlienActionButtons.js
+++ client/src/components/AlienActionButtons.js
@@ -1,11 +1,9 @@
-<<<<<<< HEAD
+
 import React from "react";
-=======
+
 import React, { useState, useEffect} from 'react';
 
 
-<<<<<<< HEAD
-=======
 // const [testState, setTestState] = useState(
 //   {
 //     'name' : '' ,
@@ -15,8 +13,7 @@ import React, { useState, useEffect} from 'react';
 //     'cooldownTimer': Date(),
 //     'createdAt': Date(),
 //     })
->>>>>>> c7c0175dbbf726eda37fb121d1f9f66704b4bd28
->>>>>>> e852024642121c01cd690aa60c322f9c211f0ea5
+
 
 function AlienActionButtons(props) {
   // const {setSelectedPage} = props
