84ce42f5aa6dfbc2f712eda2fb9a9075a4ffd607
Devyn Blackstone <devynblackmon@gmail.com>
2022-06-03 13:43:54 -0500

    something

--- client/src/pages/MainPage.js
+++ client/src/pages/MainPage.js
@@ -8,29 +8,15 @@ import landscape from "../../src/assets/landscape.png";
 // import LoginForm from "./components/LoginForm";
 import LoginPage from "./LoginPage";
 // import MainPage from "./components/MainPage"
-<<<<<<< HEAD
 import HumanActionButtons from "../components/HumanActionButtons";
 import "../styles/index.css";
+import { getUserPlants } from "../utils/plant-apis";
 import Header from "../components/Header";
 
 // In our main App component, we are rendering only single instances of Header and Navbar and several instances of Card
 function MainPage() {
   const [alienPlantPhase, setAlienPlantPhase] = useState(0);
-
   const [humanPlantPhase, setHumanPlantPhase] = useState(0);
-  return (
-    <div>
-      <Header />
-      <div className="mainPageRender">
-=======
-import HumanActionButtons from '../components/HumanActionButtons';
-import '../styles/index.css';
-import { getUserPlants } from '../utils/plant-apis';
-
-// In our main App component, we are rendering only single instances of Header and Navbar and several instances of Card
-function MainPage() {
-  const [alienPlantPhase, setAlienPlantPhase] = useState(0)
-  const [humanPlantPhase, setHumanPlantPhase] = useState(0)
 
   const [plant1, setPlant1] = useState();
   const [plant2, setPlant2] = useState();
@@ -40,44 +26,46 @@ function MainPage() {
       const getPlants = await getUserPlants("629a29249075dd2c1ac0e9d8");
 
       const userPlants = getPlants.json();
-      
-      userPlants.then(res => {
+
+      userPlants.then((res) => {
         console.log(res);
         setPlant1(res[0]);
         setPlant2(res[1]);
-      })
-    };
+      });
+    }
 
     getAndSetPlantData();
-  }, [])
+  }, []);
 
-  
-
-  return <div className="mainPageRender">
->>>>>>> c7c0175dbbf726eda37fb121d1f9f66704b4bd28
-        <div className="characterBox">
-          <Character className="character" />
-        </div>
-        <div className="alienBox">
-          <AlienPlant
-            stage={alienPlantPhase}
-            setAlienPlantPhase={setAlienPlantPhase}
-            alienPlantPhase={alienPlantPhase}
-          />
-          {/* <button id="mainButton" onClick={() => {setAlienPlantPhase(alienPlantPhase + 1)}}>Increase</button> */}
-          <AlienActionButtons />
-        </div>
-        <div className="humanBox">
-          <HumanPlant
-            stage={humanPlantPhase}
-            setHumanPlantPhase={setHumanPlantPhase}
-            humanPlantPhase={humanPlantPhase}
-          />
-          {/* <button id="mainButton" onClick={() => {setHumanPlantPhase(humanPlantPhase + 1)}}>Increase</button> */}
-          <HumanActionButtons />
+  return (
+    <>
+      <Header />
+      <div className="mainPageRender">
+        <div className="mainPageRender">
+          <div className="characterBox">
+            <Character className="character" />
+          </div>
+          <div className="alienBox">
+            <AlienPlant
+              stage={alienPlantPhase}
+              setAlienPlantPhase={setAlienPlantPhase}
+              alienPlantPhase={alienPlantPhase}
+            />
+            {/* <button id="mainButton" onClick={() => {setAlienPlantPhase(alienPlantPhase + 1)}}>Increase</button> */}
+            <AlienActionButtons />
+          </div>
+          <div className="humanBox">
+            <HumanPlant
+              stage={humanPlantPhase}
+              setHumanPlantPhase={setHumanPlantPhase}
+              humanPlantPhase={humanPlantPhase}
+            />
+            {/* <button id="mainButton" onClick={() => {setHumanPlantPhase(humanPlantPhase + 1)}}>Increase</button> */}
+            <HumanActionButtons />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
