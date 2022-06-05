d02cb20201507dae95d21290fc9f63a093ae2044
David Gardiner <97484042+Dgardi@users.noreply.github.com>
2022-06-05 19:26:22 -0400

    Update MainPage.js

--- client/src/pages/MainPage.js
+++ client/src/pages/MainPage.js
@@ -42,7 +42,7 @@ function MainPage() {
     <>
       <Header />
       <div className="mainPageRender">
-        {/* <div className="mainPageRender"> */}
+        {<div className="mainPageRender">}
           <div className="characterBox">
             <Character className="character" />
           </div>
@@ -64,7 +64,7 @@ function MainPage() {
             {/* <button id="mainButton" onClick={() => {setHumanPlantPhase(humanPlantPhase + 1)}}>Increase</button> */}
             <HumanActionButtons />
           </div>
-        {/* </div> */}
+        {</div>}
       </div>
       <Footer />
     </>
